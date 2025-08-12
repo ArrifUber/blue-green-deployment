7b651288b0190936bee475b35737a199daa1bf42
ArrifUber <arifbudianto299@gmail.com>
2025-08-12 11:49:27 +0800

    update

--- green/app.py
+++ green/app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route("/ping")
 def ping():
-    return "pong", 200
+    return "pong green", 200
 
 @app.route("/")
 def index():
-    return "Hello, World!", 200
+    return "Hello, World! green", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
