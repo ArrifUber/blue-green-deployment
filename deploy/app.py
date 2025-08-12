10a96263e799efeb761899a09372a5136f5c56a8
ArrifUber <arifbudianto299@gmail.com>
2025-08-12 22:23:05 +0800

    update

--- deploy/app.py
+++ deploy/app.py
@@ -9,7 +9,7 @@ def ping():
 
 @app.route("/")
 def index():
-    return "Hello, World!", 200
+    return "Hello, World! v1", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
